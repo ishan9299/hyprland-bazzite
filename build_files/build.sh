ac0f4b84d5f6e8acd428ced04448a2e26aca5056
ishan <ishan365.ia@gmail.com>
2026-09-15 21:14:43 +0530

    add runtime packages for systemd hyprland and quickshell

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux stow muParser tomlplusplus iniparser
+dnf5 install -y tmux stow muParser tomlplusplus iniparser jemalloc
 
 chmod +x /usr/bin/start-hyprland-session
 
