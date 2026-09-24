9273327c79c22576d82a5bf37b05ac1620351617
ishan <ishan365.ia@gmail.com>
2026-09-24 18:20:42 +0530

    fix the install command

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 -y install
+dnf5 -y install \
   qemu \
   edk2-ovmf \
   edk2-tools \
